da540ab9d17c864d8e0291ee857d4952b0d4ba32
randynyl <56857427+randynyl@users.noreply.github.com>
2021-09-12 13:07:52 +0800

    Add comments for clarity and outstanding issues

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@ std::vector<std::string> split(const std::string &line, char delimiter) {
 class Record {
 	private:
 		string tconst;
+		// float takes up less space but default is double
 		double averageRating;
 		int numVotes;
 	public:
@@ -53,15 +54,23 @@ int main(int argc, char *argv[])
 {
 	std::cout << "Program begins: " << std::endl;
 	// std::ifstream data ("./data.tsv");
-	// std::string line;
-	// while (std::getline(data, line)) {
-	// 	vector<string> row_values;
-	// 	row_values = split(line, '\t');
-	// 	for (auto v: row_values) {
-	// 		cout << v << ',';
-	// 	}
-	// 	cout << endl;
-	// }
+
+	/* adds the data from the csv into a string vector delimited by '\t'
+	std::string line;
+	while (std::getline(data, line)) {
+		vector<string> row_values;
+		row_values = split(line, '\t');
+		for (auto v: row_values) {
+			cout << v << ',';
+		}
+		cout << endl;
+	}
+	*/
+
+	/* To find out: 
+	1) why size of block does not increase after adding new records\
+	2) how to check when block has exceeded its 100B capacity to start new block
+	*/
 	Block* blockPtr = new Block();
 	cout << sizeof(*blockPtr) << endl;
 	Record record1 = Record("id", 5.0, 500);
