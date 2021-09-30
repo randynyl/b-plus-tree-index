4d5ed1140efbd6307ed02ddb76aa1379fa4e5ac0
randynyl <56857427+randynyl@users.noreply.github.com>
2021-10-01 00:38:22 +0800

    Add base insertion logic

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 int BLOCK_SIZE = 100;
 int NODE_N = 12;
+int TCONST_FIXED_LENGTH = 10;
 std::vector<std::string> split(const std::string &line, char delimiter) {
     std::stringstream ss;
     ss.str(line);
@@ -33,7 +34,8 @@ class Record {
 			this->tconst = tconst;
 			this->averageRating = averageRating;
 			this->numVotes = numVotes;
-            this->recordLength = tconst.size() + sizeof(averageRating) + sizeof(numVotes) + sizeof(int);
+            // size in bytes of tconst + averageRating + numVotes + recordLength
+            this->recordLength = TCONST_FIXED_LENGTH + sizeof(averageRating) + sizeof(numVotes) + sizeof(int);
         }
         int getRecordLength() {
             return recordLength;
@@ -48,14 +50,16 @@ class Record {
 
 class Block {
 	private:
-        // blockId as part of block header
+        // blockId and recordLength as part of block header
         int blockId;
         int recordLength;
         // records within a block are simulated to be physically contiguous.
         // records need not be sequenced in order since we're using a b+ tree index.
         vector<Record> records;
 	public:
-		Block() {}
+		Block(int id) {
+            blockId = id;
+        }
 		void* operator new(size_t size) {
             // Creating new block with allocated memory of size: BLOCK_SIZE
 			void* p = ::operator new(BLOCK_SIZE);
@@ -79,32 +83,37 @@ class Block {
 
 class Node {
     public:
-        int* keyArr;
-        Node() {
-            keyArr = new int[NODE_N];
-        }
+        vector<int> keyArr;
+        bool isLeaf;
+
+        // attributes used by LeafNode
+        vector<Record*> recordPointerArr;
+        Node* siblingNodePtr;
+
+        // attribute used my NonLeafNode
+        vector<Node*> nodePointerArr;
+
+        Node() = default;
+        virtual void split() = 0;
 };
 
 class LeafNode: public Node {
     public: 
-        Node* siblingNodePtr;
-        Record** recordPointerArr;
-
         LeafNode(): Node() {
-            recordPointerArr = new Record*[NODE_N];
+            isLeaf = true;
         }
 
-        // void insertIntoLeaf(int value, Record* recordPtr) {}
-        // void split() {}
+        // void insertIntoLeaf(int value, Record* newRecordPtr) {}
+        void split() {
+            return;
+        }
 
 };
 
 class NonLeafNode: public Node {
     public:
-        Node** nodePointerArr;
-
         NonLeafNode(): Node() {
-            nodePointerArr = new Node*[NODE_N+1];
+            isLeaf = false;
         }
 
         // void insertIntoNonLeaf(int value) {}
@@ -119,19 +128,45 @@ class BPlusTree {
             rootNode = NULL;
         }
         
-        void insertIntoTree(int value, Record* recordPtr) {
+        void insertIntoTree(int value, Record* newRecordPtr) {
             if (rootNode == NULL) {
                 LeafNode* firstNode = new LeafNode();
-                firstNode->keyArr[0] = value;
-                firstNode->recordPointerArr[0] = recordPtr;
+                firstNode->keyArr.push_back(value);
+                firstNode->recordPointerArr.push_back(newRecordPtr);
                 rootNode = firstNode;
-
-                // need to successfully check that our root node is currently a leafNode.
-                // currently, this prints 0 but it should print 1
-                cout << (typeid(*rootNode) == typeid(LeafNode)) << endl;
             } else {
                 Node* current = rootNode;
+                while (!current->isLeaf) {
+                    for (int i=0; i<current->keyArr.size(); i++) {
+                        if (value < current->keyArr[i]) {
+                            current = current->nodePointerArr[i];
+                            break;
+                        }                        
+                    }
+                    // value is greater than largest key in node, so we follow the last pointer
+                    current = current->nodePointerArr[current->keyArr.size()];
+                }
+                // current node is a leaf node
+                int sizeKeys = current->keyArr.size();
+                if (sizeKeys < NODE_N) {
+                    // finding index to insert new key
+                    int i = 0;
+                    while (current->keyArr[i] < value && i < sizeKeys) {
+                        i++;
+                    }
+                    // shifting larger keys and pointers right
+                    for (int j=sizeKeys; j>i; j--) {
+                        current->keyArr[j] = current->keyArr[j-1];
+                        current->recordPointerArr[j] = current->recordPointerArr[j-1];
+                    }
+                    current->keyArr[i] = value;
+                    current->recordPointerArr[i] = newRecordPtr;
+                } else {
+                    // node is full, need to split
+                    LeafNode* newLeafNodePtr = new LeafNode();
 
+
+                }
             }
         }
         // void deleteFromTree(int value) {}
@@ -146,7 +181,8 @@ int main(int argc, char *argv[])
 {
 	cout << "Program begins: " << std::endl;
     vector<Block> storage;
-    Block* newBlockPtr = new Block();
+    int blockId = 0;
+    Block* newBlockPtr = new Block(blockId);
     storage.push_back(*newBlockPtr);
     // begin reading tsv file and storing into blocks
 	ifstream data ("./data.tsv");
@@ -160,7 +196,8 @@ int main(int argc, char *argv[])
             int numVotes = stoi(row_values[2]);
             Record newRecord = Record(tconst, averageRating, numVotes);
             if (storage.back().getSize() + newRecord.getRecordLength() > BLOCK_SIZE) {
-                newBlockPtr = new Block();
+                blockId++;
+                newBlockPtr = new Block(blockId);
                 storage.push_back(*newBlockPtr);
             }
             storage.back().addRecord(newRecord);
